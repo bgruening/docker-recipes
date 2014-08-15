b10aa65e090deb85d357d9e0fa3d64c3e2f5ae20
Bjoern Gruening <bjoern.gruening@gmail.com>
2014-08-15 19:01:25 +0200

    Add inital Freiburg-RNA-Webserver Dockerfile

---
+++ rna-webserver/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+/etc/init.d/tomcat7 start
+
+# The container will run as long as the script is running, that's why
+# we need something long-lived here
+exec tail -f /var/log/tomcat7/catalina.out
