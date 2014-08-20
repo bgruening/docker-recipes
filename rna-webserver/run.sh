0de06569ca532bfb69f667cc78d58a0f3f6c46b5
Bjoern Gruening <bjoern.gruening@gmail.com>
2014-08-20 11:35:44 +0200

    Add gridengine configuration. Include all tools, configure tomcat.

--- rna-webserver/run.sh
+++ rna-webserver/run.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+/etc/init.d/gridengine-master start
+/etc/init.d/gridengine-exec start
+sudo -u sgeadmin qconf -am bisge001
+sudo -u bisge001 qconf -au bisge001 users
+sudo -u bisge001 qconf -Ae /exec_host
+sudo -u bisge001 qconf -Ahgrp /host_group_entry
+sudo -u bisge001 qconf -aattr hostgroup hostlist frtwebserver @allhosts
+sudo -u bisge001 qconf -Aq /queue
+sudo -u bisge001 qconf -aattr queue hostlist @allhosts main.q
+sudo -u bisge001 qconf -as frtwebserver
+
 /etc/init.d/tomcat7 start
 
 # The container will run as long as the script is running, that's why
