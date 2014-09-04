5af39b54560cbbdb582d7713670e63a99557c081
Bjoern Gruening <bjoern.gruening@gmail.com>
2014-09-04 11:33:12 +0200

    move the dynamic CPU determination into runtime and substract one

--- freiburger-rna-tools/run.sh
+++ freiburger-rna-tools/run.sh
@@ -11,6 +11,11 @@ sudo -u bisge001 qconf -Aq /queue
 sudo -u bisge001 qconf -aattr queue hostlist @allhosts main.q
 sudo -u bisge001 qconf -as frtwebserver
 
+# set maximum of avaiable CPU's
+sed -i "s|processors            4|processors            `num=$(grep ^processor /proc/cpuinfo | wc -l) && echo $((num-1))`|g" /queue
+sed -i "s|slots                 4|slots                 `num=$(grep ^processor /proc/cpuinfo | wc -l) && echo $((num-1))`|g" /queue
+
+
 /etc/init.d/tomcat7 start
 
 # The container will run as long as the script is running, that's why
